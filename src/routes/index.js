20153f6104de07b20b858a1d37df00168e78f067
Cratobi <ci.riyadh@gmail.com>
2022-08-18 13:01:12 +0600

    ...

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,18 @@
-const route = app => {
-  app.get('/', function(req, res, next) {
+const delimiter = { delimiter: '?', openDelimiter: '{{', closeDelimiter: '}}' }
+
+export default app => {
+  app.get('/login', function (req, res, next) {
+    res.render('login')
+  })
+  app.get('/search', function (req, res, next) {
+    res.render('search')
+  })
+  app.get('/event', function (req, res, next) {
+    res.render('event')
+  })
+  app.get('/', function (req, res, next) {
+    console.log('first')
     res.render('index')
+    // res.render('index', { delimiter })
   })
 }
-
-export default route
